c754d3997cf4028b9220aef12f710cdaee08db75
TheJose <devbyjose@gmail.com>
2025-05-04 18:22:52 -0500

    feat(core): implementar layout público para interfaz web de la clínica
    - Creado componente de layout público para la parte accesible sin autenticación
    - Configuradas rutas públicas para utilizar el nuevo layout
    - Implementada redirección a página 404 para rutas no encontradas
    - Corregida configuración de browserslist para compatibilidad con navegadores
    - Modificada configuración de app.config.ts para optimizar precarga

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import type { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/public-layout/public-layout.component').then(c => c.PublicLayoutComponent),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/inicio/inicio.component').then(c => c.InicioComponent),
+        pathMatch: 'full',
+        title: 'Inicio',
+      },
+      /*{
+        path: 'servicios',
+        loadComponent: () =>
+          import('./pages/servicios/servicios.component').then(c => c.ServiciosComponent),
+        title: 'Servicios',
+      },
+      {
+        path: 'doctores',
+        loadComponent: () =>
+          import('./pages/doctores/doctores.component').then(c => c.DoctoresComponent),
+        title: 'Busca tu doctor',
+      },
+      {
+        path: 'nosotros',
+        loadComponent: () =>
+          import('./pages/nosotros/nosotros.component').then(c => c.NosotrosComponent),
+        title: 'Nosotros',
+      },
+      {
+        path: 'citas',
+        loadChildren: () => import('./pages/citas/citas.routes').then(r => r.citasRoutes),
+      },*/
+      {
+        path: '404',
+        loadComponent: () =>
+          import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent),
+        title: 'Página no encontrada',
+      },
+    ],
+  },
+
+  // Rutas para los dashboards (separadas del layout público)
+  /*{
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./layout/dashboard-layout/dashboard-layout.component').then(
+        c => c.DashboardLayoutComponent
+      ),
+    loadChildren: () => import('./dashboard/dashboard.routes').then(r => r.dashboardRoutes),
+  },*/
+
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+];
