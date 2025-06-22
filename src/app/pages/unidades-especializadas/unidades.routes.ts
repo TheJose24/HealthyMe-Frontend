8c1ae8be59f476c232b87d29c4f8f87ed5242dbc
TheJose <devbyjose@gmail.com>
2025-06-22 08:15:57 -0500

    refactor(core): refactorizacion de pagina unidades especializadas
    - Se cambio la ubicacion de la pagina
    - Se refactorizo la definicion de sus rutas
    - Se  verifico la calidad de codigo

--- src/app/pages/unidades-especializadas/unidades.routes.ts
+++
@@ -1,8 +0,0 @@
-import type { Routes } from '@angular/router';
-import { EspecialidadComponent } from './especializadas.component';
-import { DetalleUnidadComponent } from './detalle-unidad.component';
-
-export const unidadesRoutes: Routes = [
-  { path: '', component: EspecialidadComponent },
-  { path: ':slug', component: DetalleUnidadComponent },
-];
